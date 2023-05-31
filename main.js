bf6e111cb1c0181d03d4ffc99c7f89546bf6d861
Danny-Satria <danny.satriaku@gmail.com>
2023-05-31 22:35:14 +0700

    Menambahkan fitur javascript

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const x = (document.getElementById("home").onclick = function () {
   document.body.style.backgroundColor = "blue";
+
+  setTimeout(() => {
+    document.body.style.backgroundColor = "";
+  }, 1000);
 });
