4b3f600511de07e24c6c6e268411c54466d2e829
Danny-Satria <danny.satriaku@gmail.com>
2023-06-05 20:10:50 +0700

    menghapus fitur main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const x = (document.getElementById("home").onclick = function () {
-  document.body.style.backgroundColor = "blue";
-
-  setTimeout(() => {
-    document.body.style.backgroundColor = "";
-  }, 1000);
-});
