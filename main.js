db7d04ab539ba9b86901bb230f333e23e5cc7861
Danny-Satria <danny.satriaku@gmail.com>
2023-06-08 23:30:22 +0700

    membuat fitur change background ketika di scroll dan mengganti contact --> contact us & mengatur posisi button our skill & let's talk

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ window.addEventListener("scroll", function () {
   let value = window.scrollY;
   console.log(value);
 });
+
+const scroll = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  if (scrollY > 150) {
+    scroll.style.background =
+      "linear-gradient(120deg,rgba(238, 9, 207, 0.5),rgb(9, 226, 161, 0.5))";
+    scroll.style.opacity = "1";
+  } else {
+    scroll.style.background = "transparent";
+  }
+});
