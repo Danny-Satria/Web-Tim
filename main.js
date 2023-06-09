1875d133c21dcdce9f72aaea25459450fbf47c3b
Danny-Satria <danny.satriaku@gmail.com>
2023-06-09 15:48:39 +0700

    membuat hamburger menu dan membuat menu navbar untuk mobile

--- main.js
+++ main.js
@@ -9,7 +9,16 @@ window.addEventListener("scroll", function () {
     scroll.style.background =
       "linear-gradient(120deg,rgba(238, 9, 207, 0.5),rgb(9, 226, 161, 0.5))";
     scroll.style.opacity = "1";
+    scroll.style.transition = "ease-in-out";
   } else {
     scroll.style.background = "transparent";
+    scroll.style.transition = "ease-in-out";
   }
+});
+
+const menuToggle = document.querySelector(".kesatuan-hamburger-menu input");
+const nav = document.querySelector(".ul-navbar");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
 });
